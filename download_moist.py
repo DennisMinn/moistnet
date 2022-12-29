64d2934c91634d59040f7322266864a21b79e009
Dennis Minn <minndennis@gmail.com>
2022-12-28 21:36:15 -0500

    added argument to download a specific playlist, only video, only audio, only transcripts and added logging for errors

--- download_moist.py
+++ download_moist.py
@@ -1,28 +1,31 @@
+import logging
 import argparse
 import pandas as pd
 from tqdm import tqdm
-from pytube import Channel
+from pytube import Channel, Playlist
 from youtube_transcript_api import YouTubeTranscriptApi
-from youtube_transcript_api.formatters import TextFormatter
-from youtube_transcript_api.formatters import JSONFormatter
+from youtube_transcript_api.formatters import TextFormatter, JSONFormatter
 from concurrent.futures import ThreadPoolExecutor
 
 
-def download_video(video):
+def download_video(video, visual, audio, transcript):
     video_id = video.video_id
     streams = video.streams
 
-    streams.get_by_itag("244").download(
-        output_path="data/video",
-        filename=f"video_{video_id}.mp4"
-    )
+    if visual:
+        streams.get_by_itag("244").download(
+            output_path="data/video",
+            filename=f"video_{video_id}.mp4"
+        )
 
-    streams.get_by_itag("139").download(
-        output_path="data/audio",
-        filename=f"audio_{video_id}.mp4"
-    )
+    if audio:
+        streams.get_by_itag("139").download(
+            output_path="data/audio",
+            filename=f"audio_{video_id}.mp4"
+        )
 
-    transcript = download_transcript(video_id)
+    if transcript:
+        transcript = download_transcript(video_id)
 
     stats = {
         "id": video_id,
@@ -51,30 +54,51 @@ def download_transcript(video_id):
 
 
 if __name__ == "__main__":
+    logging.basicConfig(
+        level=logging.DEBUG,
+        format="{asctime} {levelname:<8} {message}",
+        style="{",
+        filename=f"saved/logs/{__file__[:-3]}.log",
+        filemode="w"
+    )
+
+    logging.warning("This is a warning level msg")
     parser = argparse.ArgumentParser()
     parser.add_argument("--resume", action="store_const", const=True)
+    parser.add_argument("-v", "--visual", action="store_const", const=True, default=True)
+    parser.add_argument("-a", "--audio", action="store_const", const=True, default=True)
+    parser.add_argument("-t", "--transcript", action="store_const", const=True, default=True)
+    parser.add_argument("--playlist")
     args = parser.parse_args()
 
     if args.resume:
         ids = set(pd.read_csv("data/moist_stats.csv")["id"])
 
+    if args.playlist:
+        video_collection = Playlist(args.playlist).videos
+    else:
+        video_collection = Channel('https://www.youtube.com/user/penguinz0').videos
+
     pool = ThreadPoolExecutor(max_workers=5)
     df = []
-    moist_channel = Channel('https://www.youtube.com/user/penguinz0')
-    for video in tqdm(moist_channel.videos):
+
+    for video in tqdm(video_collection):
         if video.length / 60 > 30:
             continue
         if args.resume and video.video_id in ids:
             continue
 
         try:
-            future = pool.submit(download_video, video)
+            future = pool.submit(download_video,
+                                 video,
+                                 args.visual,
+                                 args.audio,
+                                 args.transcript)
+
             df.append(future.result())
-        except KeyError as e:
-            print(f"{e}...skipping {video.title}")
         except Exception as e:
-            print(f"{e}...stopping at video {video.title}")
-            break
+            logging.warning(f"Error at {video.video_id}")
+            logging.warning(e)
 
     pool.shutdown()
 
